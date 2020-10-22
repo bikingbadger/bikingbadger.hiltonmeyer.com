f9b080b6eee54f8e59dd88a38280b56e4c3967d5
Hilton Meyer <development@hiltonmeyer.com>
2020-10-22 06:54:33 +0300

    feat(layout): Create function for nav check in rss

--- .eleventy.js
+++ .eleventy.js
@@ -10,11 +10,20 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-   /******************* Filters ************************/
+  /******************* Filters ************************/
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
     );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  eleventyConfig.addFilter("todayDate", (dateObj) => {
+    return DateTime.local().toFormat("yyyy-LL-dd");
   });
   eleventyConfig.addFilter("head", (array, n) => {
     if (n < 0) {
@@ -24,21 +33,25 @@ module.exports = function (eleventyConfig) {
     return array.slice(0, n);
   });
 
-  eleventyConfig.addFilter("currentContent", articles => {
+  eleventyConfig.addFilter("isNavigation", (array) => {
+    if (array) {
+      return array.includes("nav") ? true : false;
+    } else {
+      return false;
+    }
+  });
+
+  eleventyConfig.addFilter("currentContent", (articles) => {
     const currentDate = new Date();
-    
-    let currentContent = articles.reverse().filter((article) =>{       
+
+    let currentContent = articles.reverse().filter((article) => {
       return article.data.date < currentDate;
-    }); 
+    });
     return currentContent;
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
-  });
   /**************** END Filters********************/
-  
+
   eleventyConfig.addCollection("tagList", require("./src/_11ty/getTagList"));
 
   eleventyConfig.addPassthroughCopy("src/assets/img");
